458da054c9a254ae84172535fec396b9266027bd
Munikiran Narpareddi <narmuni123@gmail.com>
2022-07-03 21:05:38 +0530

    added callback in to do check box

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+
+  bool isChecked = false;
+
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: const Text(
+    return  ListTile(
+      title:  Text(
         "This is a task",
+        style: TextStyle(
+          decoration: (isChecked == true) ? TextDecoration.lineThrough : null,
+        ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value){
+      trailing: TasksCheckBox(checkBoxState: isChecked,
+        toggleCheckBoxState: (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState!;
+          });
+        },),
+    );
+  }
+}
 
-        },
-      ),
+class TasksCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?) toggleCheckBoxState;
+
+  const TasksCheckBox({required this.checkBoxState, required this.toggleCheckBoxState, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
